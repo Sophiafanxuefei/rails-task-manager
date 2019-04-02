06e6fa68ffd712ff00730e25e037fed5adf3b073
sophiafan <172404042@qq.com>
2019-04-02 16:15:41 +0800

    task manager before refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,35 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new
+    @task.title = params[:task][:title]
+    @task.details = params[:task][:details]
+    @task.save!
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.title = params[:task][:title]
+    @task.details = params[:task][:details]
+    @task.completed = params[:task][:completed]
+    @task.save!
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
 end
